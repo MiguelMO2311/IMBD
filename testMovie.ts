7b41c8a217adf26f5e440e3d46c1c62ccfa16549
MiguelMO2311 <mmeneses73@gmail.com>
2023-11-14 00:25:19 +0100

    confirmacion movie

---
+++ testMovie.ts
@@ -0,0 +1,6 @@
+
+import { Professional } from './Professional'
+import {Movie} from './movie'
+
+let movie1 = new Movie ('El Rey Leon',1994,'United States of America', 'musical')
+movie1.showAllMovieData();
