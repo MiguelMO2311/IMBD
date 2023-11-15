7533d1be3e32a1e2b5945194bacc5bfef87a0dfe
MiguelMO2311 <mmeneses73@gmail.com>
2023-11-15 17:29:36 +0100

    movie

--- testMovie.ts
+++ testMovie.ts
@@ -1,5 +1,5 @@
 
-import { Professional } from './Professional'
+import { Professional } from './professional'
 import {Movie} from './movie'
 
 let movie1 = new Movie ('El Rey Leon',1994,'United States of America', 'musical')
