8730568af14dbf179fb43618bb36d6f788851d18
MiguelMO2311 <mmeneses73@gmail.com>
2023-11-17 20:13:26 +0100

    ok imdb

--- imdb.ts
+++ imdb.ts
@@ -1,26 +1,8 @@
 import { Movie } from "./movie";
 export class Imdb {
-    public films: Object[];
-    constructor(films: [{ Movie }]) {
-        this.films = [{}];
+    public films: Movie[];
+    constructor(films: Movie[]) {
+        this.films = films;
     };
 }
-// let movie1 = new Movie('El Rey Leon', 1994, 'United States of America', 'musical')
-
-// let films1 = [{
-//     title: 'Mision Imposible- Sentencia Mortal_1',
-//     releaseYear: 2023,
-//     actors: ['Tom Cruise', 'Hayley Atwell', 'Simon Pegg', 'Ving Rames'],
-//     nacionality: 'United States Of America',
-//     director: 'Christopher McQuarrie',
-//     writer: 'Christopher McQuarrie',
-//     language: 'English',
-//     plataforma: 'Prime Video',
-//     isMCU: true,
-//     mainCharacterName: 'Ethan Hunt',
-//     producer: 'Christopher McQuarrie',
-//     distributor: 'Parmount Pictures',
-//     genre: 'action'
-// }]
-
 
--- testImdb.ts
+++ testImdb.ts
@@ -1,24 +1,21 @@
 import { Movie } from './movie';
 import {Imdb} from './imdb';
+import { Professional } from './professional';
 
-let films1 = (
-    [
-    {
-    title: 'Mision Imposible- Sentencia Mortal_1',
-    releaseYear: 2023,
-    actors: ['Tom Cruise', 'Hayley Atwell', 'Simon Pegg', 'Ving Rames'],
-    nacionality: 'United States Of America',
-    director: 'Christopher McQuarrie',
-    writer: 'Christopher McQuarrie',
-    language: 'English',
-    plataforma: 'Prime Video',
-    isMCU: true,
-    mainCharacterName: 'Ethan Hunt',
-    producer: 'Christopher McQuarrie',
-    distributor: 'Parmount Pictures',
-    genre: 'action'
-}
-]
-);
+let movie1 = new Movie ('El Rey Leon',1994,'United States of America', 'musical', )
+let movie2 = new Movie ('Mision Imposible- Sentencia Mortal_1', 2023, 'United States of America','action')
+let director1 = new Professional ('Pepe', 45, 56, 1.60, true, 'Spanish', 8, 'director');
+let director2 = new Professional('Christopher McQuarrie', 54, 73, 1.83,true, 'American',3,'director')
+let writer1 = new Professional ('Juan', 45, 56, 1.60, true, 'Spanish', 8, 'director');
+let writer2 = new Professional ('Jhon Waine', 39, 65, 1.90, true, 'Indian', 8, 'freelance');
+let actor1 = new Professional('Juan Carlos', 45, 56, 1.60, true, 'Spanish', 8, 'director');
+let actor2 = new Professional('Antonio Banderas', 63, 70, 1.70, true, 'Spanish', 1, 'actor')
+movie1.actors.push(actor1);
+movie2.actors.push(actor2),
+movie1.director = director1;
+movie2.director = director2;
+movie1.writer = writer1;
+movie2.writer = writer2;
 
-console.log (films1)
+let film1 = new Imdb ([movie1, movie2]);
+console.log (film1)
